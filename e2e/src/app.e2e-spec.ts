479e708d4f471f66de84c22cc87047ab92b8c3d9
Akash Kansara <akash.kansara96@gmail.com>
2020-04-03 14:21:54 +0530

    name update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-bootstrap-boilerplate app is running!');
+    expect(page.getTitleText()).toEqual('angular-starter app is running!');
   });
 
   afterEach(async () => {
